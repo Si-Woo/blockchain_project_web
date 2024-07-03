3408266067c0b334ba5b7f0c7f9962149e9e7125
Si-Woo <Si-Woo@users.noreply.github.com>
2024-07-03 02:27:38 +0000

    deploy: b5b51b365e455ee73ac73c175d79a10acd2b951f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1398657906"
+    serviceWorkerVersion: "3743312045"
   }
 });
